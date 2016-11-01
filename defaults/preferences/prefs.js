272a8c7288fc34fa081e7dc1986f4e2715439c78
Garoe <garoedp@gmail.com>
2016-11-01 21:49:09 +0000

    Initial commit
    Copy from version 0.6 of Thunderkeep, source code at
    https://addons.mozilla.org/en-US/thunderbird/addon/thunderkeep/

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,3 @@
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.thunderkeep@jensheuschkel.de.description", "chrome://thunderkeep/locale/overlay.properties");
+pref("extensions.thunderkeep@jensheuschkel.de.installComplete", false);
