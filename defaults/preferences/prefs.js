cbbba5b5402f96dabca3259fc5281b24c5ed64f6
Garoe <garoedp@gmail.com>
2016-11-09 22:19:14 +0000

    Removed preferences in pref file

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,4 +1,1 @@
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.thunderkeepplus@gmail.com.description", "chrome://thunderkeepplus/locale/overlay.properties");
-pref("extensions.thunderkeepplus@gmail.com.installComplete", false);
-pref("extensions.thunderkeepplus@gmail.com.googleKeepTabId", "");
+pref("extensions.thunderkeepplus.googleKeepTabId", "");
