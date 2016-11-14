89c5b1449c1c39b2d3492023e11f6ceb1f245974
Garoe <garoedp@gmail.com>
2016-11-14 21:58:49 +0000

    Removed all debug instances

--- content/overlay.js
+++ content/overlay.js
@@ -13,63 +13,42 @@ Cu.import("resource://gre/modules/devtools/Console.jsm");
 
 var TKPManager = function()
 {
-	this.enableDebug = false;
 	this.prompt = Cc["@mozilla.org/embedcomp/prompt-service;1"].getService(Ci.nsIPromptService);
 	this.strings = Services.strings.createBundle('chrome://ThunderKeepPlus/locale/overlay.properties?' + Math.random());
 	this.mailPane = null;
 	this.tabManager = null;
 	this.tabsArray = null;
 }
-TKPManager.prototype.debug= function (aMessage) {
-	if(this.enableDebug) {
-		let consoleService = Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService);
-		consoleService.logStringMessage("ThunderKeepPlus: " + aMessage);
-	}
-}
 TKPManager.prototype.onLoad = function(document)
 {
-	try{
-		this.debug("TKPManager onLoad");
-		
+	try{		
 		let customButton = document.getElementById("thunderkeepplus-toolbar-button");
 		
 		var self = this;
 		customButton.addEventListener("click", function(event) {
         	self.onToolbarButtonClick(event);
     	});
-    	
-    	this.debug("tabTitle1 is:\"" + this.strings.GetStringFromName('ThunderKeepPlus.tabTitle1') + 
-			"\" and 2 is:\"" + this.strings.GetStringFromName('ThunderKeepPlus.tabTitle2') + "\"");
-			
+    				
 		this.mailPane = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator).getMostRecentWindow("mail:3pane");
 		this.tabManager = this.mailPane.document.getElementById("tabmail");
 		this.tabsArray = this.tabManager.tabInfo;
 
-		this.debug("TKPManager onLoad successful");
 	} catch(e) { this.prompt.alert(null, "ThunderKeepPlus Error", "onLoad: " + e);}
 }
 TKPManager.prototype.onUnload = function()
 {
-	this.debug("TKPManager onUnLoad");
 	// Close the Google Keep tab
-	try{		
-		this.debug("Found " + String(this.tabsArray.length) + " tabs");
-		
+	try{			
 		for (let i = 0; i < this.tabsArray.length; i++) {
 			let tabBrowser = this.tabsArray[i].browser;
 			if(tabBrowser){
-				this.debug("Tab " + i +  " with id \"" + tabBrowser.id + "\" and title \"" + this.tabsArray[i].title + "\"");
 				if(this.tabsArray[i].title === this.strings.GetStringFromName('ThunderKeepPlus.tabTitle1')
 					|| this.tabsArray[i].title === this.strings.GetStringFromName('ThunderKeepPlus.tabTitle2')){
-					this.debug("Closing tab " + i);
 					this.tabManager.closeTab(i);
 					return;
 				}
-			} else {
-				this.debug("Tab " + i + " without id and title \"" + this.tabsArray[i].title + "\"");
 			}
 		}
-		this.debug("TKPManager onUnLoad successful");
 	} catch(e) { this.prompt.alert(null, "ThunderKeepPlus Error", "onUnload: "+ e );}
 }
 TKPManager.prototype.onToolbarButtonClick = function(event) {
@@ -80,29 +59,19 @@ TKPManager.prototype.onToolbarButtonClick = function(event) {
 		if(event.button !== 0){
 			return;
 		}
-		this.debug("Found " + String(this.tabsArray.length) + " tabs");
-		
 		for (let i = 0; i < this.tabsArray.length; i++) {
 			let tabBrowser = this.tabsArray[i].browser;
 			if(tabBrowser){
-				this.debug("Tab " + i +  " with id \"" + tabBrowser.id + "\" and title \"" + this.tabsArray[i].title + "\"");
 				if(this.tabsArray[i].title === this.strings.GetStringFromName('ThunderKeepPlus.tabTitle1')
 					|| this.tabsArray[i].title === this.strings.GetStringFromName('ThunderKeepPlus.tabTitle2')){
-					this.debug("Switching to tab " + i);
 					this.tabManager.switchToTab(i);
 					return;
 				}
-			} else {
-				this.debug("Tab " + i + " without id and title \"" + this.tabsArray[i].title + "\"");
 			}
 		}
-		
-		this.debug("Tab no found, opening new one");
-		
+				
 		let gtab = this.tabManager.openTab("contentTab", {contentPage: "http://keep.google.com"});
-		
-		this.debug("Tab opened successfully");
-		
+				
 	} catch(e) { this.prompt.alert(null, "ThunderKeepPlus Error", "onToolbarButtonClick: "+ e );}
 }
 
